0a8e1adba811fe1cf5b2e6b8df604c42f80cce48
Alex <globophobe@gmail.com>
2020-01-20 00:46:46 +0900

    first commit

---
+++ presentation/theme/index.js
@@ -0,0 +1,60 @@
+import createTheme from "../../createTheme";
+
+const colors = {
+  primary: "white",
+  secondary: "#22211f", // almost black w/ blueish tint
+  tertiary: "#1F2022", // complement of secondary
+  quaternary: "#808080" // pale gray
+};
+
+const marginBottom = "0.5em";
+
+const theme = createTheme(
+  colors,
+  {
+    primary: "Montserrat",
+    secondary: "Helvetica"
+  },
+  {
+    progress: {
+      pacmanTop: {
+        background: colors.quaternary
+      },
+      pacmanBottom: {
+        background: colors.quaternary
+      },
+      point: {
+        borderColor: colors.quaternary
+      }
+    },
+    components: {
+      heading: {
+        h1: {
+          fontSize: "5vw",
+          marginBottom
+        },
+        h2: {
+          fontSize: "4vw",
+          marginBottom
+        },
+        h3: {
+          fontSize: "3vw"
+        },
+        h4: {
+          fontSize: "2vw"
+        },
+        h5: {
+          fontSize: "1vw"
+        },
+        h6: {
+          fontSize: "1vw"
+        }
+      },
+      codePane: {
+        fontSize: "1.25rem"
+      }
+    }
+  }
+);
+
+export default theme;
